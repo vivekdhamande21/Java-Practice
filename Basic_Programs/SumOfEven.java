0710a924f0da47c38f8c55ba03fcb1f8cb344222
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-07-20 20:01:04 +0000

    Sum of first even numbers

---
+++ Basic_Programs/SumOfEven.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class SumOfEven{
+	public static void main(String[] args){
+		System.out.println("Chhose n : ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+		int i = 1;
+		while(n>0){
+			if(i%2==0){
+				sum = sum + i;
+				n--;
+			}
+			i++;
+		}
+		System.out.println(sum);
+	}
+}
