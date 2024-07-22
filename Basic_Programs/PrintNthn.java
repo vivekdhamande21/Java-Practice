3bef2ea345df364b3c9ca197920abdb42e93fd08
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-07-22 10:12:56 +0000

    Print Nth n

---
+++ Basic_Programs/PrintNthn.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class PrintNthn{
+	public static void main(String[] args){
+		System.out.println("Chhose n : ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int i = 5;
+		while(n>0){
+			if(i%2==1){
+				n--;				
+			}
+			i++;
+		}
+		System.out.println(i-1);
+	}
+}
