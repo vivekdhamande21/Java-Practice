2f561fdf92714a095505461b99c820831a004d9e
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-07-25 18:01:31 +0000

    Print A to Z

---
+++ Basic_Programs/PrintAlphabets.java
@@ -0,0 +1,9 @@
+/*Write a program to print all alphabets from a to z*/
+class PrintAlphabets{
+	public static void main(String[] args){
+        char c;
+  		for( c = 'A' ; c <= 'Z' ; c++ ) {
+            System.out.println(c + " ");
+        }
+    }
+}
