4b5b383d78f80256b1aa7404dde924a0e4b2d91d
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-07-31 18:29:24 +0000

    Proper Divisor of number

---
+++ Basic_Programs/Factor.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Factor{
+	public static void main(String[] args){
+  		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number");
+		int number = sc.nextInt();
+        System.out.println("Enter Power");
+           
+    }
+}
---
+++ Basic_Programs/ProperDivisor.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class ProperDivisor{
+	public static void main(String[] args){
+  		Scanner sc = new Scanner(System.in);
+		  System.out.println("Enter Number");
+		  int number = sc.nextInt();
+          int i = 1;
+          while(i < number){
+            if(number%i == 0){
+                System.out.println(i);
+            }
+            i++;
+          }
+	}	
+}
