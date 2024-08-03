802615e3a85cc11cb1f81a0cf270313f5ca5c283
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-08-03 17:21:48 +0000

    Pattern programs

---
+++ Basic_Programs/Pattern1.java
@@ -0,0 +1,31 @@
+/* write a function in java to print following pattern
+Note: Number of rows and columns shall be the input parameters 
+to the function
+Rectangle
+****
+****
+****
+****
+****
+
+*/
+import java.util.*;
+class Pattern1{
+	public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row");
+        int row = sc.nextInt();
+        System.out.println("Enter row");
+        int col = sc.nextInt();
+        int colTemp = col;
+  		while(row > 0) { 
+            col = colTemp;
+            while(col > 0){
+                System.out.print("*");
+                col--;
+            }
+            System.out.println("");
+            row--;
+        }        
+	}	
+}
--- Basic_Programs/ProperDivisor.java
+++ Basic_Programs/ProperDivisor.java
@@ -4,12 +4,12 @@ class ProperDivisor{
   		Scanner sc = new Scanner(System.in);
 		  System.out.println("Enter Number");
 		  int number = sc.nextInt();
-          int i = 1;
-          while(i < number){
-            if(number%i == 0){
-                System.out.println(i);
-            }
-            i++;
-          }
-	}	
+      int i = 1;
+      while(i < number){
+        if(number%i == 0){
+          System.out.println(i);
+        }
+        i++;
+      }
+  }	
 }
