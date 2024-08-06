365f1ba59cfe2b8a2c8ba984803102d55b7050af
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-08-06 18:02:33 +0000

    Pattern 4

--- Basic_Programs/Pattern4.java
+++ Basic_Programs/Pattern4.java
@@ -2,15 +2,16 @@
 Note: Number of rows and columns shall be the input parameters 
 to the function
 Rectangle
-****
-*  *
-*  *
-*  *
-****
+
+    *****
+   *****
+  *****
+ *****
+*****
 
 */
 import java.util.*;
-class Pattern2{
+class Pattern4{
 	public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter row");
@@ -18,19 +19,26 @@ class Pattern2{
         System.out.println("Enter col");
         int col = sc.nextInt();
         int colTemp = col;
+        int i = 4;
   		while(row > 0) { 
             col = colTemp;
-            while(col > 0){
-                if((row > 1 && row < 5) && (col > 1 && col < 4)){
+            while(col + i >= 0){
+                if(col > 0 && ((col + i) > 4)){
                     System.out.print(" ");
                 }
                 else{
-                    System.out.print("*");
+                     if((row < 5 && row > 1 ) && ((col + i) < 4  && (col + i) > 0)){
+                        System.out.print(" ");
+                     }
+                    else{
+                        System.out.print("*");
+                    }
                 }
                 col--;
             }
             System.out.println("");
             row--;
+            i--;
         }        
 	}	
 }
