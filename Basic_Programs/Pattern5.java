b6156fbeb6f51377e42d3d05b6b155c9f6fea39a
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-08-07 16:30:02 +0000

    Pattern 5

---
+++ Basic_Programs/Pattern5.java
@@ -0,0 +1,39 @@
+/* write a function in java to print following pattern
+Note: Number of rows and columns shall be the input parameters 
+to the function
+Rectangle
+
+*****
+ *****
+  *****
+   *****
+    *****
+
+*/
+import java.util.*;
+class Pattern5{
+	public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row");
+        int row = sc.nextInt();
+        System.out.println("Enter col");
+        int col = sc.nextInt();
+        int colTemp = col;
+        int i = 0;
+  		while(row > 0) { 
+            col = colTemp;
+            while(col + i >= 0){
+                if(col + i <= 5){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                col--;
+            }
+            System.out.println("");
+            row--;
+            i++;
+        }        
+	}	
+}
