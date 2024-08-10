476885ed8c28ae363b43cdc517903420ff28d046
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-08-10 15:40:16 +0000

    Pattern 11

---
+++ Basic_Programs/Pattern11.java
@@ -0,0 +1,41 @@
+/*write a function in java to print following pattern
+Note: Only number of rows shall be the input parameter to the 
+function
+Hollow inverted right triangle
+*****
+*  *
+* *
+**
+*
+
+*/
+
+import java.util.*;
+class Pattern11{
+	public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row");
+        int row = sc.nextInt();
+        int rowTemp = row;
+        int i;
+  		while(row > 0) { 
+            i  = 1;
+            int j=0;
+            while( rowTemp >= i){
+                if(i<=row){
+                    if((i>1&&i<row) && (row<rowTemp))
+                        System.out.print(" ");
+                    else
+                        System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                i++;
+            }
+            System.out.println("");
+            row--;
+            j++;
+        }        
+	}	
+}
