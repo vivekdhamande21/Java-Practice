cbf14349ddf35cdaa402978ce27809ca418e94c0
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-08-14 16:45:30 +0000

    Pattern 14

---
+++ Basic_Programs/Pattern14.java
@@ -0,0 +1,39 @@
+/*write a function in java to print following pattern
+Note: Only number of rows shall be the input parameter to the 
+function
+Hollow inverted right triangle
+
+   *
+  * *
+ *   *
+*******
+
+*/
+
+import java.util.*;
+class Pattern14{
+	public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row");
+        int row = sc.nextInt();
+        int rowTemp = row;
+        int i,j=0;
+  		while(row > 0) { 
+            i  = 1;
+            while( i <= ((rowTemp*2) -1) ){
+                if(((i==rowTemp+j) || (i ==rowTemp-j)))
+                    System.out.print("*");
+                else{
+                    if(row==1)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                i++;
+            }
+            System.out.println("");
+            row--;
+            j++;
+        }        
+	}	
+}
