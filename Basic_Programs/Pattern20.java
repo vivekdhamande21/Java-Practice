a6a1f4393c61ba9b8d240e80b76c2e3b6d59f0f0
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-08-24 09:27:46 +0000

    Pattern 20

---
+++ Basic_Programs/Pattern20.java
@@ -0,0 +1,46 @@
+/*write a function in Java to print following pattern
+Note: Only number of columns will be the input parameter to the 
+function
+Mirrored half diamond
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+ 
+ */
+
+ import java.util.*;
+ class Pattern20{
+     public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter row");
+         int row = sc.nextInt();
+         int rowTemp = row;
+         int i,j=(row/2 + 1),k=0,l=0;
+           while(row>0){ 
+             i  = 1;
+             while( i <= rowTemp){     
+                 if(i>(j-l) && i < (j+l) && (row > 1 && row < rowTemp)  )
+                     System.out.print(" ");
+                 else{
+                     System.out.print("*");
+                 }   
+                 i++;
+             }
+             System.out.println("");
+             row--;
+             if(l == j-1){
+                 k=1;
+             }
+             if (k==1) {
+                 l--;
+             }
+             else{
+                 l++;
+             }
+         }        
+     }	
+ }
