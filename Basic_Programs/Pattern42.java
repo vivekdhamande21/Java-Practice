7a2cee311821c94ea2ac80e7e9486c4d068acfdb
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-09-29 18:24:18 +0000

    Pattern 42

--- Basic_Programs/Pattern42.java
+++ Basic_Programs/Pattern42.java
@@ -2,28 +2,31 @@
 Note: Only number of columns will be the input parameter to the 
 function
 Mirrored half diamond
-55555
-4444
+11111
+2222
 333
-22
-1
+44
+5
  */
 
  import java.util.*;
- class Pattern41{
+ class Pattern42{
     public static void main(String[] args){
          Scanner sc = new Scanner(System.in);
          System.out.println("Enter row");
          int row = sc.nextInt();
+         int rowTemp = row;
          int i;
+         int j=1;
          while(row>0){
             i=1;
             while(i<=row){
-                System.out.print(row);
+                System.out.print(j);
                 i++;
             }
             System.out.println("");
             row--;
+            j++;
          }    
     }  
           
