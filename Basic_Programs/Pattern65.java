76212626b97cc0f9cf2065d906e947ee3d6a37b1
Vivek Dhamande <113935640+vivekdhamande21@users.noreply.github.com>
2024-10-24 02:42:58 +0000

    Pattern 65

--- Basic_Programs/Pattern65.java
+++ Basic_Programs/Pattern65.java
@@ -2,28 +2,37 @@
 Note: Only number of columns will be the input parameter to the 
 function
 Mirrored half diamond
+
  */
 
  import java.util.*;
- class Pattern64{
+ class Pattern65{
     public static void main(String[] args){
          Scanner sc = new Scanner(System.in);
          System.out.println("Enter row");
          int row = sc.nextInt();
          int i = 1;
-         int j = 1;
+         int j;
          int k = 1;
          while(row>0){
-            i = 1;
-            while(i<=j){
-                System.out.print(" "+k);
-                i++;
-                k++;
+            i=1;
+            j=k;
+            if(j%2==1){
+                while(i<=j){
+                    System.out.print(i);
+                    i++;
+                }
+            }
+            else{
+                while(j>0){
+                    System.out.print(j);
+                    j--;
+                }
             }
             System.out.println();
-            j++;
+            k++;
             row--;
-         }     
+        }     
    }  
           
 }
